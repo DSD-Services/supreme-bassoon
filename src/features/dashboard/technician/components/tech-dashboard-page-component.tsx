7ddace16caab69a1e7c5849c18c0c6703c731c6e
Katie Hom <schaka01@gmail.com>
2025-03-08 16:25:00 -0700

    feat: Create technician dashboard page component (DSD-42)

---
+++ src/features/dashboard/technician/components/tech-dashboard-page-component.tsx
@@ -0,0 +1,10 @@
+import WorkOrderList from './work-order-list';
+
+export default function TechnicianDashboardPageComponent() {
+  return (
+    <>
+      <div>Technician Dashboard</div>
+      <WorkOrderList />
+    </>
+  );
+}
