b6d324c276762e8a60d7f1d7ab7c2b604ebeda4f
Katie Hom <schaka01@gmail.com>
2025-03-09 10:06:35 -0600

    feat: Restructure folder/file organization (DSD-51)

--- src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import HomePageComponent from "@/features/home/components/home-page-component";
-
-export default function Page() {
-  return (
-    <>
-      <HomePageComponent />
-    </>
-  );
-}
--- src/features/dashboard/technician/components/small-label.tsx
+++
@@ -1,9 +0,0 @@
-import { ReactNode } from "react";
-
-interface SmallLabelProps {
-  children: ReactNode;
-}
-
-export default function SmallLabel({ children }: SmallLabelProps) {
-  return <span className="text-primary text-xs font-medium">{children}</span>;
-}
