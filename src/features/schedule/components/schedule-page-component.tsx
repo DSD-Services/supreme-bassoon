7ec8e8474c8738ca724f25c05a44b0b6d8effa7c
Katie Hom <schaka01@gmail.com>
2025-03-10 15:23:05 -0600

    feat: Create schedule page component (DSD-62)

---
+++ src/features/schedule/components/schedule-page-component.tsx
@@ -0,0 +1,10 @@
+import ScheduleWorkOrderForm from "./schedule-work-order-form";
+
+export default function SchedulePageComponent() {
+  return (
+    <section>
+      <h1>Schedule a Service Appointment</h1>
+      <ScheduleWorkOrderForm />
+    </section>
+  );
+}
