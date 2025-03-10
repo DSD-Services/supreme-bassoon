fd06cbbc9ba7ff219e9ecd877964ce90e8043164
Katie Hom <schaka01@gmail.com>
2025-03-10 15:26:06 -0600

    feat: Create calendar types (DSD-62)

---
+++ src/features/schedule/types/calendar.types.ts
@@ -0,0 +1,24 @@
+export interface Timeslot {
+  title: string;
+  start: Date | string;
+  end: Date | string;
+  id: number;
+}
+
+export interface Appointment {
+  title: string;
+  start: Date | string;
+  end: Date | string;
+  id: number;
+  extendedProps: {
+    department: string;
+    serviceType: string;
+    technician: string;
+    customerName: string;
+  };
+}
+
+export type WorkOrderStep =
+  | "appointment"
+  | "completeWorkOrder"
+  | "confirmation";
