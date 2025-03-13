28b266ebb19be49f7ae6fe2df21e09cb1a09666b
Katie Hom <schaka01@gmail.com>
2025-03-13 15:21:52 -0600

    feat: Fetch timeslots, remove fetch technicians add state (DSD-62)

--- src/features/schedule/components/schedule-work-order-form.tsx
+++ src/features/schedule/components/schedule-work-order-form.tsx
@@ -1,59 +1,55 @@
 // TODO -- onSubmit will need to be typed like the form data
+// <WorkOrderFormData>
 // TODO -- verify that required fields are filled out and validated
 //    before moving to next step of form
 // TODO -- render errors beneath inputs with error
 //    message using React hook form
+// TODO -- display dates in user timezone
 
 "use client";
 
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { SubmitHandler, useForm } from "react-hook-form";
 import {
   Department,
   DepartmentsResponse,
   ServiceType,
   ServiceTypesResponse,
-  Technician,
-  TechniciansResponse,
 } from "../types/backend-data";
 import StepButtons from "./step-buttons";
 import ScheduleWorkOrderCalendar from "./schedule-work-order-calendar";
-import { backgroundEvents, availableTimeslots } from "../lib/events";
 import { EventClickArg } from "@fullcalendar/core/index.js";
-import { Timeslot } from "../types/calendar.types";
+import { BackgroundEvent } from "../types/calendar.types";
 import { Dialog } from "@/components/ui/dialog";
-import { useDialog } from "@/components/ui/use-dialog";
 import { Button } from "@/components/ui/button";
 import { formatDateLong } from "@/lib/utils";
+import { Select } from "@/components/ui/select";
+import { groupTimeslotsByDay } from "../lib/generate-timeslots";
+import { Timeslot } from "@/lib/types/work-order-types";
 
 export default function ScheduleWorkOrderForm() {
   const [step, setStep] = useState(1);
   const [departments, setDepartments] = useState<Department[] | []>([]);
+  const [isLoading, setIsLoading] = useState(false);
   const [serviceTypes, setServiceTypes] = useState<ServiceType[]>([]);
   const [isDisabled, setIsDisabled] = useState(true);
   const [isNextDisabled, setIsNextDisabled] = useState(false);
   const [isBackDisabled, setIsBackDisabled] = useState(false);
-  const [departmentTechnicians, setDepartmentTechnicians] = useState<
-    Technician[] | []
+  const [backgroundEvents, setBackgroundEvents] = useState<
+    BackgroundEvent[] | []
   >([]);
   const [selectedDate, setSelectedDate] = useState<Date | null>(null);
+  const [allTimeslots, setAllTimeslots] = useState<Timeslot[]>([]);
   const [filteredSlots, setFilteredSlots] = useState<Timeslot[]>([]);
   const [isTimeslotModalOpen, setIsTimeslotModalOpen] = useState(false);
   const [selectedSlot, setSelectedSlot] = useState<Timeslot | null>(null);
   const [todaySelected, setTodaySelected] = useState(false);
+  const [isOverflowing, setIsOverflowing] = useState(false);
 
-  const {
-    register,
-    handleSubmit,
-    reset,
-    onChange,
-    formState: { errors },
-    watch,
-  } = useForm();
-
-  const { isOpen, openDialog, onClose } = useDialog();
-
+  const { register, handleSubmit, reset, watch } = useForm();
   const watchDepartmentSelect = watch("departmentId");
+
+  const timeslotListRef = useRef<HTMLUListElement>(null);
 
   useEffect(() => {
     const fetchDepartments = async () => {
@@ -77,6 +73,9 @@ export default function ScheduleWorkOrderForm() {
 
   useEffect(() => {
     const fetchServiceTypes = async (departmentId: number) => {
+      if (!watchDepartmentSelect) return;
+
+      setIsLoading(true);
       try {
         const response = await fetch(
           `/api/service-types/by-department/${departmentId}`,
@@ -87,33 +86,13 @@ export default function ScheduleWorkOrderForm() {
         setServiceTypes(serviceTypes.data);
       } catch (error) {
         console.error("Error fetching service types", error);
+      } finally {
+        setIsLoading(false);
+        setIsDisabled(false);
       }
     };
 
-    if (watchDepartmentSelect) {
-      setIsDisabled(false);
-      fetchServiceTypes(watchDepartmentSelect);
-    }
-  }, [watchDepartmentSelect]);
-
-  useEffect(() => {
-    const fetchTechnicians = async (departmentId: number) => {
-      try {
-        const response = await fetch(
-          `/api/technicians/by-department/${departmentId}`,
-        );
-        if (!response.ok) throw new Error("Failed to fetch technicians");
-
-        const technicians: TechniciansResponse = await response.json();
-        setDepartmentTechnicians(technicians);
-      } catch (error) {
-        console.error("Error fetching technicians", error);
-      }
-    };
-
-    if (watchDepartmentSelect) {
-      fetchTechnicians(watchDepartmentSelect);
-    }
+    fetchServiceTypes(watchDepartmentSelect);
   }, [watchDepartmentSelect]);
 
   const nextStep = () => setStep((prev) => prev + 1);
@@ -123,6 +102,33 @@ export default function ScheduleWorkOrderForm() {
     }
     setStep((prev) => prev - 1);
   };
+
+  useEffect(() => {
+    const fetchTimeslots = async () => {
+      if (!watchDepartmentSelect) return;
+
+      try {
+        const response = await fetch(
+          `/api/technicians/by-department/${watchDepartmentSelect}/schedules`,
+        );
+        if (!response.ok) throw new Error("Failed to fetch timeslots");
+
+        const { timeslots }: { timeslots: Timeslot[] } = await response.json();
+
+        setAllTimeslots(timeslots);
+      } catch (error) {
+        console.error("Error fetching timeslots", error);
+      }
+    };
+
+    fetchTimeslots();
+  }, [watchDepartmentSelect]);
+
+  useEffect(() => {
+    if (!allTimeslots) return;
+    const backgroundEvents = groupTimeslotsByDay(allTimeslots);
+    setBackgroundEvents(backgroundEvents);
+  }, [allTimeslots]);
 
   const handleDateClick = (arg: { date: Date }) => {
     const clickedDate = arg.date.toISOString().split("T")[0];
@@ -134,19 +140,42 @@ export default function ScheduleWorkOrderForm() {
       setTodaySelected(false);
     }
 
-    const slots = availableTimeslots.filter((slot) =>
-      slot.start.startsWith(clickedDate),
-    );
-    const sortedSlots = slots.sort(
-      (a, b) => new Date(a.start).getTime() - new Date(b.start).getTime(),
-    );
-    console.log("Arg.date:", arg.date);
     console.log("Clicked Date:", clickedDate);
-    console.log("Time slots for date clicked", sortedSlots);
+
+    const slots = allTimeslots.filter((slot) => {
+      if (!slot.start) return false;
+      const slotDate = new Date(slot.start);
+      return (
+        !isNaN(slotDate.getTime()) &&
+        slotDate.toISOString().startsWith(clickedDate)
+      );
+    });
+
+    const sortedSlots = slots.sort((a, b) => {
+      return new Date(a.start).getTime() - new Date(b.start).getTime();
+    });
+
     setSelectedDate(arg.date);
     setFilteredSlots(sortedSlots);
     setIsTimeslotModalOpen(true);
   };
+
+  useEffect(() => {
+    if (timeslotListRef.current) {
+      const isOverflowing =
+        timeslotListRef.current.scrollHeight >
+        timeslotListRef.current.clientHeight;
+      setIsOverflowing(isOverflowing);
+    }
+  }, [filteredSlots]);
+
+  useEffect(() => {
+    if (isTimeslotModalOpen) {
+      document.body.classList.add("modal-open");
+    } else {
+      document.body.classList.remove("modal-open");
+    }
+  }, [isTimeslotModalOpen]);
 
   const handleEventClick = (info: EventClickArg) => {
     info.jsEvent.preventDefault();
@@ -170,6 +199,7 @@ export default function ScheduleWorkOrderForm() {
   const handleSelectSlot = (slot: Timeslot) => {
     console.log("Timeslot clicked:", slot);
     setSelectedSlot(slot);
+    setIsTimeslotModalOpen(false);
     setStep(3);
   };
 
@@ -193,7 +223,7 @@ export default function ScheduleWorkOrderForm() {
               <label htmlFor="department" className="text-sm text-blue-800">
                 Department
               </label>
-              <select
+              <Select
                 {...register("departmentId", { required: true })}
                 className="rounded-lg border-r-10 border-transparent bg-white px-3 py-2 text-sm shadow-lg hover:cursor-pointer md:text-base"
               >
@@ -203,24 +233,26 @@ export default function ScheduleWorkOrderForm() {
                     {department.name}
                   </option>
                 ))}
-              </select>
+              </Select>
             </div>
             <div className="flex flex-col pt-6">
               <label htmlFor="serviceType" className="text-sm text-blue-800">
                 Service Type
               </label>
-              <select
+              <Select
                 {...register("serviceTypeId", { required: true })}
                 className={`rounded-lg border-r-10 border-transparent px-3 py-2 text-sm shadow-lg md:text-base ${isDisabled ? "bg-slate-300 text-slate-600" : "bg-white hover:cursor-pointer"}`}
                 disabled={isDisabled}
               >
-                <option value="">-- Please select a service --</option>
+                <option value="">
+                  {isLoading ? "Loading..." : "-- Please select a service --"}
+                </option>
                 {serviceTypes.map((service) => (
                   <option key={service.id} value={service.id}>
                     {service.name}
                   </option>
                 ))}
-              </select>
+              </Select>
             </div>
             <div className="mt-4 flex justify-between gap-10">
               <Button type="button" variant="destructive" onClick={handleReset}>
@@ -237,8 +269,8 @@ export default function ScheduleWorkOrderForm() {
         {step === 2 && (
           <>
             <div className="flex flex-col items-center justify-center px-2">
-              <h2 className="pb-2 text-center text-lg font-semibold">
-                Step 2: Select an available date and time
+              <h2 className="pb-1 text-center text-lg font-semibold">
+                Step 2: Click on an available date highlighted in green
               </h2>
               <div className="mb-2 w-full max-w-[400px] rounded-lg bg-white p-2 shadow-lg md:mx-4 lg:mx-10">
                 <ScheduleWorkOrderCalendar
@@ -248,9 +280,6 @@ export default function ScheduleWorkOrderForm() {
                 />
               </div>
               <div className="flex flex-col items-center">
-                <span className="text-base font-semibold italic">
-                  Click on any date highlighted in green.
-                </span>
                 <span className="block pb-2 text-sm italic">
                   Use the buttons at the top of the calendar to navigate if
                   needed.
@@ -279,25 +308,38 @@ export default function ScheduleWorkOrderForm() {
                   </span>
                 </div>
               ) : filteredSlots.length > 0 ? (
-                <ul className="pb-4">
-                  {filteredSlots.map((slot: Timeslot) => (
-                    <li
-                      key={slot.id}
-                      className="my-2 cursor-pointer rounded bg-blue-100 p-2 text-black hover:bg-blue-300"
-                      onClick={() => handleSelectSlot(slot)}
-                    >
-                      {new Date(slot.start).toLocaleTimeString([], {
-                        hour: "2-digit",
-                        minute: "2-digit",
-                      })}{" "}
-                      -{" "}
-                      {new Date(slot.end).toLocaleTimeString([], {
-                        hour: "2-digit",
-                        minute: "2-digit",
-                      })}
-                    </li>
-                  ))}
-                </ul>
+                <>
+                  <span className="block text-center text-lg font-semibold">
+                    Select an available timeslot
+                  </span>
+                  {isOverflowing && (
+                    <span className="block pb-2 text-center text-sm italic">
+                      Scroll to see more available times
+                    </span>
+                  )}
+                  <ul
+                    ref={timeslotListRef}
+                    className="inner-shadow mb-6 max-h-72 overflow-y-auto pb-4"
+                  >
+                    {filteredSlots.map((slot: Timeslot) => (
+                      <li
+                        key={slot.id}
+                        className="mx-4 my-2 cursor-pointer rounded bg-blue-100 p-2 text-black hover:bg-blue-300"
+                        onClick={() => handleSelectSlot(slot)}
+                      >
+                        {new Date(slot.start).toLocaleTimeString([], {
+                          hour: "2-digit",
+                          minute: "2-digit",
+                        })}{" "}
+                        -{" "}
+                        {new Date(slot.end).toLocaleTimeString([], {
+                          hour: "2-digit",
+                          minute: "2-digit",
+                        })}
+                      </li>
+                    ))}
+                  </ul>
+                </>
               ) : (
                 <div className="pb-4">
                   <span className="block text-center text-black">
