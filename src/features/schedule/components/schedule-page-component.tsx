4097676ea2a3411e6c1e986717aaa5b8d98ec3bf
Katie Hom <schaka01@gmail.com>
2025-03-13 07:37:15 -0600

    feat: Delete heading on schedule page (DSD-62)

--- src/features/schedule/components/schedule-page-component.tsx
+++ src/features/schedule/components/schedule-page-component.tsx
@@ -3,7 +3,6 @@ import ScheduleWorkOrderForm from "./schedule-work-order-form";
 export default function SchedulePageComponent() {
   return (
     <section>
-      <h1>Schedule a Service Appointment</h1>
       <ScheduleWorkOrderForm />
     </section>
   );
