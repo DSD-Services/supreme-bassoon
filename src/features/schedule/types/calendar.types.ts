9815ba4d69a3d7d16f8d16027cc987e0627e6564
Katie Hom <schaka01@gmail.com>
2025-03-13 15:23:40 -0600

    feat: Delete timeslot type and modify bg event type (DSD-62)

--- src/features/schedule/types/calendar.types.ts
+++ src/features/schedule/types/calendar.types.ts
@@ -1,12 +1,5 @@
 export const APPOINTMENT_LEAD_TIME = 28;
 
-export interface Timeslot {
-  title: string;
-  start: Date | string;
-  end: Date | string;
-  id: number;
-}
-
 export interface Appointment {
   title: string;
   start: Date | string;
@@ -23,6 +16,4 @@ export interface BackgroundEvent {
   id: string;
   start: string;
   end: string;
-  display?: string;
-  classNames?: string[];
 }
