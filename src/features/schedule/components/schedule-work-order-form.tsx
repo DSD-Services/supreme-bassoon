394bcd547f271eacb7180f570759b1121fe7ea00
Katie Hom <schaka01@gmail.com>
2025-03-13 08:02:34 -0600

    feat: Add reset, add timeslot logic, edit styles, add dialog, complete step 2 (DSD-62)

--- src/features/schedule/components/schedule-work-order-form.tsx
+++ src/features/schedule/components/schedule-work-order-form.tsx
@@ -7,7 +7,7 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import { useForm } from "react-hook-form";
+import { SubmitHandler, useForm } from "react-hook-form";
 import {
   Department,
   DepartmentsResponse,
@@ -18,15 +18,21 @@ import {
 } from "../types/backend-data";
 import StepButtons from "./step-buttons";
 import ScheduleWorkOrderCalendar from "./schedule-work-order-calendar";
-import { backgroundEvents, availableTimeslots } from "../data/events";
+import { backgroundEvents, availableTimeslots } from "../lib/events";
 import { EventClickArg } from "@fullcalendar/core/index.js";
 import { Timeslot } from "../types/calendar.types";
+import { Dialog } from "@/components/ui/dialog";
+import { useDialog } from "@/components/ui/use-dialog";
+import { Button } from "@/components/ui/button";
+import { formatDateLong } from "@/lib/utils";
 
 export default function ScheduleWorkOrderForm() {
   const [step, setStep] = useState(1);
   const [departments, setDepartments] = useState<Department[] | []>([]);
   const [serviceTypes, setServiceTypes] = useState<ServiceType[]>([]);
   const [isDisabled, setIsDisabled] = useState(true);
+  const [isNextDisabled, setIsNextDisabled] = useState(false);
+  const [isBackDisabled, setIsBackDisabled] = useState(false);
   const [departmentTechnicians, setDepartmentTechnicians] = useState<
     Technician[] | []
   >([]);
@@ -34,15 +40,18 @@ export default function ScheduleWorkOrderForm() {
   const [filteredSlots, setFilteredSlots] = useState<Timeslot[]>([]);
   const [isTimeslotModalOpen, setIsTimeslotModalOpen] = useState(false);
   const [selectedSlot, setSelectedSlot] = useState<Timeslot | null>(null);
+  const [todaySelected, setTodaySelected] = useState(false);
 
   const {
     register,
     handleSubmit,
-    getValues,
+    reset,
     onChange,
     formState: { errors },
     watch,
   } = useForm();
+
+  const { isOpen, openDialog, onClose } = useDialog();
 
   const watchDepartmentSelect = watch("departmentId");
 
@@ -103,24 +112,39 @@ export default function ScheduleWorkOrderForm() {
     };
 
     if (watchDepartmentSelect) {
-      console.log(
-        "Watch department select from inside useEffect:",
-        watchDepartmentSelect,
-      );
       fetchTechnicians(watchDepartmentSelect);
     }
   }, [watchDepartmentSelect]);
 
   const nextStep = () => setStep((prev) => prev + 1);
-  const prevStep = () => setStep((prev) => prev - 1);
+  const prevStep = () => {
+    if (step === 3) {
+      setSelectedSlot(null);
+    }
+    setStep((prev) => prev - 1);
+  };
 
   const handleDateClick = (arg: { date: Date }) => {
     const clickedDate = arg.date.toISOString().split("T")[0];
-    const slots = availableTimeslots.filter((slot) => {
-      slot.start.startsWith(clickedDate);
-    });
+    const todayDate = new Date().toISOString().split("T")[0];
+
+    if (clickedDate === todayDate) {
+      setTodaySelected(true);
+    } else {
+      setTodaySelected(false);
+    }
+
+    const slots = availableTimeslots.filter((slot) =>
+      slot.start.startsWith(clickedDate),
+    );
+    const sortedSlots = slots.sort(
+      (a, b) => new Date(a.start).getTime() - new Date(b.start).getTime(),
+    );
+    console.log("Arg.date:", arg.date);
+    console.log("Clicked Date:", clickedDate);
+    console.log("Time slots for date clicked", sortedSlots);
     setSelectedDate(arg.date);
-    setFilteredSlots(slots);
+    setFilteredSlots(sortedSlots);
     setIsTimeslotModalOpen(true);
   };
 
@@ -129,30 +153,49 @@ export default function ScheduleWorkOrderForm() {
     console.log("Event clicked:", info.event.start);
   };
 
-  const onSubmit = (data) => {
-    console.log(data);
+  const handleReset = () => {
+    reset({
+      departmentId: "",
+      serviceTypeId: "",
+    });
+    setIsDisabled(true);
+    setServiceTypes([]);
   };
 
-  const formatDate = (date: Date): string => {
-    return date.toLocaleDateString("en-US", {
-      weekday: "long",
-      year: "numeric",
-      month: "long",
-      day: "numeric",
-    });
+  const handleCloseTimeslotModal = () => {
+    setIsTimeslotModalOpen(false);
+    setSelectedSlot(null);
+  };
+
+  const handleSelectSlot = (slot: Timeslot) => {
+    console.log("Timeslot clicked:", slot);
+    setSelectedSlot(slot);
+    setStep(3);
+  };
+
+  // TODO - Create Work Order Form Data type
+  const onSubmit: SubmitHandler<WorkOrderFormData> = (data) => {
+    console.log("Submitting:", data);
   };
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <div className="bg-secondary mx-2 my-6 rounded-lg p-2 pb-6 shadow-xl md:mx-4 lg:mx-10">
+    <form
+      onSubmit={handleSubmit(onSubmit)}
+      className="flex min-h-[500px] justify-center"
+    >
+      <div className="my-2 flex h-1/2 w-11/12 justify-center rounded-lg bg-blue-100 pt-3 pb-6">
         {step === 1 && (
-          <>
-            <h2>Step 1: Select Department & Service</h2>
+          <div className="w-11/12 sm:w-3/4 md:w-3/5 lg:w-1/2">
+            <h2 className="text-center text-lg font-semibold">
+              Step 1: Select Department & Service
+            </h2>
             <div className="flex flex-col pt-6">
-              <label htmlFor="department">Department</label>
+              <label htmlFor="department" className="text-sm text-blue-800">
+                Department
+              </label>
               <select
                 {...register("departmentId", { required: true })}
-                className="rounded-lg shadow-lg hover:cursor-pointer"
+                className="rounded-lg border-r-10 border-transparent bg-white px-3 py-2 text-sm shadow-lg hover:cursor-pointer md:text-base"
               >
                 <option value="">-- Please select a department --</option>
                 {departments.map((department) => (
@@ -163,10 +206,12 @@ export default function ScheduleWorkOrderForm() {
               </select>
             </div>
             <div className="flex flex-col pt-6">
-              <label htmlFor="serviceType">Service Type</label>
+              <label htmlFor="serviceType" className="text-sm text-blue-800">
+                Service Type
+              </label>
               <select
                 {...register("serviceTypeId", { required: true })}
-                className={`rounded-lg shadow-lg ${isDisabled ? "bg-slate-300 text-slate-600" : "bg-white hover:cursor-pointer"}`}
+                className={`rounded-lg border-r-10 border-transparent px-3 py-2 text-sm shadow-lg md:text-base ${isDisabled ? "bg-slate-300 text-slate-600" : "bg-white hover:cursor-pointer"}`}
                 disabled={isDisabled}
               >
                 <option value="">-- Please select a service --</option>
@@ -177,134 +222,157 @@ export default function ScheduleWorkOrderForm() {
                 ))}
               </select>
             </div>
-            <StepButtons type="nextOnly" nextStep={nextStep} />
-          </>
+            <div className="mt-4 flex justify-between gap-10">
+              <Button type="button" variant="destructive" onClick={handleReset}>
+                Reset
+              </Button>
+              <StepButtons
+                variant="nextOnly"
+                nextStep={nextStep}
+                isNextDisabled={isNextDisabled}
+              />
+            </div>
+          </div>
         )}
         {step === 2 && (
           <>
-            <div className="flex flex-col items-center justify-center py-6">
-              <h2>Step 2: Select an available date</h2>
-              <div className="w-full max-w-[400px] rounded-lg bg-white p-2 shadow-lg md:mx-4 lg:mx-10">
+            <div className="flex flex-col items-center justify-center px-2">
+              <h2 className="pb-2 text-center text-lg font-semibold">
+                Step 2: Select an available date and time
+              </h2>
+              <div className="mb-2 w-full max-w-[400px] rounded-lg bg-white p-2 shadow-lg md:mx-4 lg:mx-10">
                 <ScheduleWorkOrderCalendar
                   backgroundEvents={backgroundEvents}
                   handleDateClick={handleDateClick}
                   handleEventClick={handleEventClick}
                 />
               </div>
-              <StepButtons
-                type="prevNext"
-                prevStep={prevStep}
-                nextStep={nextStep}
-              />
-            </div>
-            {isTimeslotModalOpen && (
-              <div className="bg-opacity-50 fixed inset-0 z-50 flex items-center justify-center bg-black/40">
-                <div className="w-[90%] max-w-md rounded-lg bg-white p-6 shadow-lg">
-                  <h2 className="text-center">Select an available timeslot:</h2>
-                  {selectedDate && (
-                    <h3 className="mb-4 text-center">
-                      {formatDate(selectedDate)}
-                    </h3>
-                  )}
-                  {filteredSlots.length > 0 ? (
-                    <ul>
-                      {filteredSlots.map((slot: Timeslot) => (
-                        <li
-                          key={slot.id}
-                          className="my-2 cursor-pointer rounded bg-gray-100 p-2 text-black hover:bg-gray-300"
-                          onClick={() => handleSelectSlot(slot)}
-                        >
-                          {new Date(slot.start).toLocaleTimeString([], {
-                            hour: "2-digit",
-                            minute: "2-digit",
-                          })}{" "}
-                          -
-                          {new Date(slot.end).toLocaleTimeString([], {
-                            hour: "2-digit",
-                            minute: "2-digit",
-                          })}
-                        </li>
-                      ))}
-                    </ul>
-                  ) : (
-                    <div className="rounded-lg bg-white py-10">
-                      <span className="block text-center text-black">
-                        No available slots for this day.
-                      </span>
-                      <span className="block text-center">
-                        Please go back and select another day.
-                      </span>
-                    </div>
-                  )}
-                  <div className="mt-4 flex justify-between">
-                    <button
-                      onClick={() => setIsTimeslotModalOpen(false)}
-                      className="rounded bg-gray-300 p-2"
-                    >
-                      Cancel
-                    </button>
-                    {/* <button
-                      onClick={() => nextStep()}
-                      className="rounded bg-blue-500 p-2 text-white"
-                    >
-                      Confirm & Continue
-                    </button> */}
-                  </div>
-                </div>
+              <div className="flex flex-col items-center">
+                <span className="text-base font-semibold italic">
+                  Click on any date highlighted in green.
+                </span>
+                <span className="block pb-2 text-sm italic">
+                  Use the buttons at the top of the calendar to navigate if
+                  needed.
+                </span>
+
+                <StepButtons variant="prevOnly" prevStep={prevStep} />
               </div>
-            )}
+            </div>
+            <Dialog
+              isOpen={isTimeslotModalOpen}
+              onClose={handleCloseTimeslotModal}
+              title={
+                selectedDate
+                  ? formatDateLong(selectedDate)
+                  : "Select an available timeslot"
+              }
+            >
+              {todaySelected ? (
+                <div className="pb-4 text-center">
+                  <span className="font-medium">You selected today.</span>
+                  <span className="block">
+                    If you need a same-day appointment,{" "}
+                    <span className="block">
+                      please call our office to check availability.
+                    </span>
+                  </span>
+                </div>
+              ) : filteredSlots.length > 0 ? (
+                <ul className="pb-4">
+                  {filteredSlots.map((slot: Timeslot) => (
+                    <li
+                      key={slot.id}
+                      className="my-2 cursor-pointer rounded bg-blue-100 p-2 text-black hover:bg-blue-300"
+                      onClick={() => handleSelectSlot(slot)}
+                    >
+                      {new Date(slot.start).toLocaleTimeString([], {
+                        hour: "2-digit",
+                        minute: "2-digit",
+                      })}{" "}
+                      -{" "}
+                      {new Date(slot.end).toLocaleTimeString([], {
+                        hour: "2-digit",
+                        minute: "2-digit",
+                      })}
+                    </li>
+                  ))}
+                </ul>
+              ) : (
+                <div className="pb-4">
+                  <span className="block text-center text-black">
+                    Unfortunately, no times are available on this day.
+                  </span>
+                  <span className="block text-center">
+                    Please go back and select another day.
+                  </span>
+                </div>
+              )}
+              <div className="flex justify-center">
+                <StepButtons
+                  variant="prevOnly"
+                  prevStep={handleCloseTimeslotModal}
+                />
+              </div>
+            </Dialog>
           </>
         )}
         {step === 3 && (
-          <div className="z-50 bg-white">
-            <h2 className="text-center">
-              Step 3: Select an available timeslot:
+          <div className="flex flex-col items-center justify-center px-2">
+            <h2 className="flex pb-2 text-center text-lg font-semibold">
+              Step 3: Confirm selected appointment time:
             </h2>
-            {selectedDate && (
-              <h3 className="mb-4 text-center">{formatDate(selectedDate)}</h3>
-            )}
-            {filteredSlots.length > 0 ? (
-              <ul>
-                {filteredSlots.map((slot: Timeslot) => (
-                  <li
-                    key={slot.id}
-                    className="my-2 cursor-pointer rounded bg-gray-100 p-2 text-black hover:bg-gray-300"
-                    onClick={() => handleSelectSlot(slot)}
-                  >
-                    {new Date(slot.start).toLocaleTimeString([], {
-                      hour: "2-digit",
+            {selectedSlot && (
+              <div className="pb-6 text-center">
+                <div className="rounded-md bg-white p-4">
+                  <span className="block font-semibold text-blue-800">
+                    Selected Time:
+                  </span>
+                  <span className="block text-lg font-medium">
+                    {formatDateLong(new Date(selectedSlot.start))}
+                  </span>
+                  <span className="block text-lg font-medium">
+                    {new Date(selectedSlot.start).toLocaleTimeString([], {
+                      hour: "numeric",
                       minute: "2-digit",
+                      hour12: true,
                     })}{" "}
-                    -
-                    {new Date(slot.end).toLocaleTimeString([], {
-                      hour: "2-digit",
+                    -{" "}
+                    {new Date(selectedSlot.end).toLocaleTimeString([], {
+                      hour: "numeric",
                       minute: "2-digit",
+                      hour12: true,
                     })}
-                  </li>
-                ))}
-              </ul>
-            ) : (
-              <div className="rounded-lg bg-white py-10">
-                <span className="block text-center text-black">
-                  No available slots for this day.
-                </span>
-                <span className="block text-center">
-                  Please go back and select another day.
-                </span>
+                  </span>
+                </div>
+                <p className="pt-4 italic">
+                  If the date and time above look correct, please proceed to the
+                  next step.
+                </p>
+                <p className="italic">
+                  If not, please go back and select a different date or time.
+                </p>
               </div>
             )}
             <StepButtons
-              type="prevNext"
+              variant="prevNext"
               prevStep={prevStep}
               nextStep={nextStep}
             />
           </div>
         )}
+        {step === 4 && (
+          <div>
+            <h2 className="flex pb-2 text-center text-lg font-semibold">
+              Step 4: Confirm your contact information:
+            </h2>
+            <div className="flex justify-center gap-10">
+              <StepButtons variant="prevOnly" prevStep={prevStep} />
+              <Button type="submit">Submit</Button>
+            </div>
+          </div>
+        )}
       </div>
     </form>
   );
-}
-
-{
-  /* <button type="submit">Submit</button> */
 }
