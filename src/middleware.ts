2d47947442ba9926833ae597f45b2f38897ec2eb
rudzzz <rudsolza13@gmail.com>
2025-03-14 00:14:44 -0300

    fix: allow access to login and register pages in middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,7 +3,7 @@ import { NextResponse, type NextRequest } from "next/server";
 import type { Database } from "./utils/supabase/types/database.types.ts";
 
 export async function middleware(request: NextRequest) {
-  let supabaseResponse = NextResponse.next({ request });
+  const response = NextResponse.next();
 
   const supabase = createServerClient<Database>(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
@@ -15,27 +15,27 @@ export async function middleware(request: NextRequest) {
           cookiesToSet.forEach(({ name, value, options }) => {
             request.cookies.set(name, value);
           });
-          supabaseResponse = NextResponse.next({ request });
           cookiesToSet.forEach(({ name, value, options }) => {
-            supabaseResponse.cookies.set(name, value, options);
+            response.cookies.set(name, value, options);
           });
         },
       },
     },
   );
 
-  // Allow public access only to the home page
-  const isHomePage = request.nextUrl.pathname === "/";
+  // Allow public access only to specific pages
+  const publicPages = ["/", "/login", "/register"];
+  const isPublicPage = publicPages.includes(request.nextUrl.pathname);
 
   // Fetch authenticated user
   const { data, error } = await supabase.auth.getUser();
 
-  // If the user is NOT logged in and tries to access a restricted page (anything other than the home page), redirect them to the home page.
-  if (!isHomePage && (error || !data?.user)) {
+  // If the user is NOT logged in and tries to access a protected page, redirect them
+  if (!isPublicPage && (error || !data?.user)) {
     return NextResponse.redirect(new URL("/", request.url));
   }
 
-  return supabaseResponse;
+  return response;
 }
 
 export const config = {
