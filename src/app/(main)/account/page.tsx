a27c09906ce74450a4a37f8c9cea236c45ae34e1
rudzzz <rudsolza13@gmail.com>
2025-03-14 20:39:38 -0300

    feat: Style account page, fix footer height issue

--- src/app/(main)/account/page.tsx
+++ src/app/(main)/account/page.tsx
@@ -21,104 +21,71 @@ export default async function Page() {
   }
 
   return (
-    <div>
-      <h1>Account</h1>
+    <div className="mx-auto my-8 max-w-3xl border-2 p-6 font-sans">
+      <h1 className="mb-6 text-center text-3xl font-semibold">Account</h1>
 
-      <hr />
+      <h2 className="text-2xl font-medium text-gray-700">Profile</h2>
 
-      <h2>Profile</h2>
-
-      <ul>
+      <ul className="mt-4 list-none space-y-2 text-gray-600">
         <li>
-          <strong>id:</strong> {profile.id}
+          <strong>First Name::</strong> {profile.first_name}
         </li>
         <li>
-          <strong>created_at:</strong> {profile.created_at}
+          <strong>Last Name:</strong> {profile.last_name}
         </li>
         <li>
-          <strong>updated_at:</strong> {profile.updated_at}
+          <strong className="text-gray-600">Address:</strong>{" "}
+          {profile.address_line1}, {profile.address_line2}, {profile.city},{" "}
+          {profile.state}, {profile.postal_code}
         </li>
         <li>
-          <strong>role:</strong> {profile.role}
+          <strong>Primary Phone::</strong> {profile.primary_phone}
         </li>
         <li>
-          <strong>first_name:</strong> {profile.first_name}
-        </li>
-        <li>
-          <strong>last_name:</strong> {profile.last_name}
-        </li>
-        <li>
-          <strong>address_line1:</strong> {profile.address_line1}
-        </li>
-        <li>
-          <strong>address_line2:</strong> {profile.address_line2}
-        </li>
-        <li>
-          <strong>city:</strong> {profile.city}
-        </li>
-        <li>
-          <strong>state:</strong> {profile.state}
-        </li>
-        <li>
-          <strong>postal_code:</strong> {profile.postal_code}
-        </li>
-        <li>
-          <strong>primary_phone:</strong> {profile.primary_phone}
-        </li>
-        <li>
-          <strong>secondary_phone:</strong> {profile.secondary_phone}
+          <strong>Secondary Phone:</strong> {profile.secondary_phone}
         </li>
       </ul>
 
       {technicianDetails ? (
         <>
-          <hr />
+          <hr className="my-6" />
 
-          <h2>Technician Details</h2>
+          <h2 className="text-2xl font-medium text-gray-700">
+            Technician Details
+          </h2>
 
-          <ul>
+          <ul className="mt-4 list-none space-y-4 text-gray-600">
             <li>
-              <strong>id:</strong> {technicianDetails.id}
+              <strong>Work Days: </strong>
+              {typeof technicianDetails.work_days === "string"
+                ? technicianDetails.work_days
+                    .match(
+                      /(MONDAY|TUESDAY|WEDNESDAY|THURSDAY|FRIDAY|SATURDAY|SUNDAY)/gi,
+                    )
+                    ?.map((day) => day.charAt(0) + day.slice(1).toLowerCase())
+                    .join(", ") || technicianDetails.work_days
+                : Array.isArray(technicianDetails.work_days)
+                  ? technicianDetails.work_days
+                      .map((day) => day.charAt(0) + day.slice(1).toLowerCase())
+                      .join(", ")
+                  : technicianDetails.work_days}
             </li>
+
             <li>
-              <strong>created_at:</strong> {technicianDetails.created_at}
-            </li>
-            <li>
-              <strong>updated_at:</strong> {technicianDetails.updated_at}
-            </li>
-            <li>
-              <strong>work_start_time:</strong>{" "}
-              {technicianDetails.work_start_time}
-            </li>
-            <li>
-              <strong>work_end_time:</strong> {technicianDetails.work_end_time}
-            </li>
-            <li>
-              <strong>work_days:</strong> {technicianDetails.work_days}
-            </li>
-            <li>
-              <strong>break_start_time:</strong>{" "}
-              {technicianDetails.break_start_time}
-            </li>
-            <li>
-              <strong>break_end_time:</strong>{" "}
-              {technicianDetails.break_end_time}
-            </li>
-            <li>
-              <strong>department_id:</strong> {technicianDetails.department_id}
-            </li>
-            <li>
-              <strong>departments:</strong>{" "}
-              {technicianDetails.departments?.name}
+              <strong>Time: </strong>
+              {technicianDetails.work_start_time} -{" "}
+              {technicianDetails.break_start_time} /
+              {technicianDetails.break_end_time} -{" "}
+              {technicianDetails.work_end_time}
             </li>
           </ul>
         </>
       ) : null}
 
-      <hr />
+      <hr className="my-6" />
 
       <form action="/api/auth/signout" method="post">
-        <Button variant="destructive" type="submit">
+        <Button variant="destructive" type="submit" className="w-full">
           Sign out
         </Button>
       </form>
