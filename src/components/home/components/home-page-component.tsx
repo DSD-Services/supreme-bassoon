aeee4ccb22a8cfbe0c54c0d0261c06457b62df23
rudzzz <rudsolza13@gmail.com>
2025-03-16 12:00:40 -0300

    feat: Add How It Works Section to Home Page

--- src/components/home/components/home-page-component.tsx
+++ src/components/home/components/home-page-component.tsx
@@ -1,12 +1,14 @@
 import DepartmentSection from "./department-section";
 import GetStartedSection from "./get-started-section";
 import HomeHero from "./home-hero";
+import HowItWorks from "./how-it-works";
 
 export default function HomePageComponent() {
   return (
     <>
       <HomeHero />
       <DepartmentSection />
+      <HowItWorks />
       <GetStartedSection />
     </>
   );
