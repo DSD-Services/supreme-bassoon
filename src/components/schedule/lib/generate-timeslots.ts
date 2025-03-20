0c7d873921043ce0e32fa69a4add8d7a4547aefb
Katie Hom <schaka01@gmail.com>
2025-03-20 07:50:15 -0600

    refactor: Utilize Luxon for workday (DSD-151)

--- src/components/schedule/lib/generate-timeslots.ts
+++ src/components/schedule/lib/generate-timeslots.ts
@@ -7,16 +7,6 @@ import type {
   Timeslot,
 } from "@/lib/types/work-order-types";
 
-// const dayOfWeekMap: { [key: number]: WorkDay } = {
-//   0: "SUNDAY",
-//   1: "MONDAY",
-//   2: "TUESDAY",
-//   3: "WEDNESDAY",
-//   4: "THURSDAY",
-//   5: "FRIDAY",
-//   6: "SATURDAY",
-// };
-
 export function generateTimeslots(
   technicians: Technician[],
   appointments: Appointment[],
