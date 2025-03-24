d88bb94d818db4f2cec8119a23ceeaec41f5a581
Khurram Ali <khurramcali@gmail.com>
2025-03-23 20:22:47 -0400

    add emails for admin acc creation

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -8,7 +8,7 @@ export function cn(...inputs: ClassValue[]) {
 
 export function capitalize(str: string) {
   if (!str) return "";
-  return str.charAt(0).toUpperCase() + str.slice(1);
+  return str.toLowerCase().charAt(0).toUpperCase() + str.slice(1);
 }
 
 export function formatTime(time: string) {
