391b554c85add01517bb564fd5b6f3177aea8900
Maye Jesuorobo <nayawes@yahoo.com>
2025-03-26 17:02:57 +0100

    Added the findTechnicianAppointment and findTechnicianSchedule functions

--- src/features/technician-details/queries.ts
+++ src/features/technician-details/queries.ts
@@ -15,3 +15,35 @@ export async function findOneTechnicianDetails(userId: string) {
     .eq("id", userId)
     .single();
 }
+
+export async function findTechnicianSchedule(userId: string) {
+  const supabase = await createClient();
+
+  const { data, error } = await supabase
+    .from("technician_details")
+    .select("*")
+    .eq("id", userId);
+
+  if (error) {
+    console.error("[findTechnicianScheduleError]:", error.message);
+    return null;
+  }
+
+  return data;
+}
+
+export async function findTechnicianAppointments(userId: string) {
+  const supabase = await createClient();
+
+  const { data, error } = await supabase
+    .from("work_orders")
+    .select("id, appointment_start, appointment_end, job_details, department_id, service_type_id")
+    .eq("technician_id", userId);
+
+  if (error) {
+    console.error("[findTechnicianAppointmentsError]:", error.message);
+    return null;
+  }
+
+  return data;
+}
